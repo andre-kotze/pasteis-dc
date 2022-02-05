16a3ead849035805bbe1784c52fcd6600e0ba889
Andre Kotze <andre@nammail.net>
2022-02-05 18:38:46 +0000

    fixes

--- populate.sql
+++ populate.sql
@@ -1,4 +1,4 @@
 -- Insert into warehouses tables
-INSERT INTO warehouses ("wkb_geometry" , "id", "name", "addressline1", "addressline2", "city", "country", "postalcode", "stock",) 
-VALUES ('0101000020E6100000CE54D13C5F3822C0DC321BEBEF5C4340', 'Armazem das Docas Locas', 'Rua da Cintura do Porto de Lisboa, 230', ,'Lisboa', 'Portugal', '1950-323',400000);
+INSERT INTO warehouses ("geom" , "id", "name", "addressline1", "city", "country", "postalcode", "stock") 
+VALUES ('0101000020E6100000CE54D13C5F3822C0DC321BEBEF5C4340', '1', 'Armazem das Docas Locas', 'Rua da Cintura do Porto de Lisboa, 230','Lisboa', 'Portugal', '1950-323',400000);
 COMMIT;
