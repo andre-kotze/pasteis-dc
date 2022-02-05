7d924aa52fd9ca38c9ed45df09b44abf746e8734
Guillem Ulldemolins i Jornet <guillemullde@gmail.com>
2022-02-05 17:44:44 +0000

    Armazem data updated

--- populate.sql
+++ populate.sql
@@ -1,4 +1,4 @@
 -- Insert into warehouses tables
 INSERT INTO warehouses (
     id, name, addressline1, addressline2, postalcode, stock) 
-VALUES ('1', 'San Francisco', '100 Market Street', 'Penthouse', '420-69', 400000);
+VALUES ('1', 'Armazem das Docas Locas', 'Rua da Cintura do Porto de Lisboa, 230', 'Lisboa - Portugal', '1950-323', 400000);
