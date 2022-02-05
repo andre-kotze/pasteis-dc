270ed7374cda7555e6f20855e11962c9655001f8
Guillem Ulldemolins i Jornet <guillemullde@gmail.com>
2022-02-05 17:55:51 +0000

    Mais uma

--- populate.sql
+++ populate.sql
@@ -1,4 +1,4 @@
 -- Insert into warehouses tables
 INSERT INTO warehouses (
-    id, name, addressline1, addressline2, postalcode, stock) 
-VALUES ('1', 'Armazem das Docas Locas', 'Rua da Cintura do Porto de Lisboa, 230', 'Lisboa - Portugal', '1950-323', 400000);
+    id, name, addressline1, addressline2, city, country, postalcode, geom, stock) 
+VALUES ('1', 'Armazem das Docas Locas', 'Rua da Cintura do Porto de Lisboa, 230', ,'Lisboa', 'Portugal', '1950-323', '',400000);
