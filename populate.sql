4ae77bb02bf9214ce2fe8fea3a42018d8720587c
Andre Kotze <andre@nammail.net>
2022-02-05 17:24:13 +0000

    andre commit populate sql

---
+++ populate.sql
@@ -0,0 +1,4 @@
+-- Insert into warehouses tables
+INSERT INTO warehouses (
+    id, name, addressline1, addressline2, postalcode, stock) 
+VALUES ('1', 'San Francisco', '100 Market Street', 'Penthouse', '420-69', 400000);
