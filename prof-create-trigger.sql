1b69fbdf89e66641f2e76e2a76eed0cbeecfa18e
Andre Kotze <andre@nammail.net>
2022-02-14 18:03:53 +0000

    comm

--- prof-create-trigger.sql
+++ prof-create-trigger.sql
@@ -2,13 +2,14 @@
     The purpose of the trigger is to insert into the pa.rides every time a new record is inserted
     in sa.rides. Plus, the longitude and latitude are converted into PostGIS datatypes.
 */
-CREATE FUNCTION sa.insert_ride_in_pa()
+CREATE FUNCTION pasteis.update_orders()
 RETURNS TRIGGER AS
 $$
 BEGIN
 
 
 	new.geom = (SELECT geom from clients as c where c.client_id = new.client_id)
+    new.client = (SELECT client_name from clients as c where c.client_id = new.client_id)
 
 
 END;
@@ -16,10 +17,13 @@ $$
 LANGUAGE 'plpgsql';
 
 CREATE TRIGGER insert_rides_in_pa
-BEFORE INSERT ON sa.rides
+BEFORE INSERT ON pasteis.orders
 FOR EACH ROW
-EXECUTE PROCEDURE sa.insert_ride_in_pa();
+EXECUTE PROCEDURE pasteis.update_orders();
 
+
+/* THIS IS THE SECOND OPTION MAYBE FOR IMPLEMENTING LATER......
 create view order_geom as
 select o.*, c.geom as geom
 from order as o join clients as c on (c.client_id = o.client_id)
+*/
